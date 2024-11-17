f6d41e1be09c9a73aed692f0fb2c6081519c09dc
Soulaiman Bouhlal <bouhlal.soulaiman@gmail.com>
2024-11-17 17:14:44 +0100

    - Define CRUD operation contracts - Add search method signature - Document service methods"

---
+++ src/main/java/com/javangers/citronix/service/FarmService.java
@@ -0,0 +1,23 @@
+package com.javangers.citronix.service;
+
+
+import com.javangers.citronix.web.params.FarmSearchParams;
+import com.javangers.citronix.web.vm.request.FarmRequestVM;
+import com.javangers.citronix.web.vm.response.FarmResponseVM;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+
+import java.util.Optional;
+import java.util.UUID;
+
+
+public interface FarmService {
+    FarmResponseVM createFarm(FarmRequestVM farmRequest);
+    FarmResponseVM getFarm(UUID id);
+    FarmResponseVM updateFarm(UUID id, FarmRequestVM farmRequest);
+    void deleteFarm(UUID id);
+    Page<FarmResponseVM> searchFarms(FarmSearchParams searchParams, Pageable pageable);
+}
+
+
